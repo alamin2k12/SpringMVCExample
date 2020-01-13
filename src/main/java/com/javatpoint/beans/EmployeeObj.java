dfa2cc60d6b4eef47607044c46f4d0271fd2be5c
ahossain <ahossain@lrglobalbd.com>
2020-01-13 11:47:09 +0600

    First Commit at master

---
+++ src/main/java/com/javatpoint/beans/EmployeeObj.java
@@ -0,0 +1,17 @@
+package com.javatpoint.beans;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class EmployeeObj {
+  List <Emp> employeeList=new ArrayList<Emp>();
+
+public List<Emp> getEmployeeList() {
+	return employeeList;
+}
+
+public void setEmployeeList(List<Emp> employeeList) {
+	this.employeeList = employeeList;
+}
+  
+}
---
+++ src/main/java/com/javatpoint/controllers/EmpController.java
@@ -0,0 +1,96 @@
+package com.javatpoint.controllers;   
+import java.util.ArrayList;
+import java.util.List;  
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;  
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;  
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.javatpoint.beans.Emp;
+import com.javatpoint.beans.EmployeeObj;
+import com.javatpoint.dao.EmpDao;  
+@RestController 
+public class EmpController {  
+    
+    EmpDao dao;//will inject dao from xml file  
+      
+    /*It displays a form to input data, here "command" is a reserved request attribute 
+     *which is used to display object data into form 
+     */  
+    @RequestMapping("/empform")  
+    public String showform(Model m){  
+    	m.addAttribute("command", new Emp());
+    	return "empform"; 
+    }  
+    /*It saves object into database. The @ModelAttribute puts request data 
+     *  into model object. You need to mention RequestMethod.POST method  
+     *  because default request is GET*/  
+    @RequestMapping(value="/save",method = RequestMethod.POST)  
+    public String save(@ModelAttribute("emp") Emp emp){  
+        dao.save(emp);  
+        return "redirect:/viewemp";//will redirect to viewemp request mapping  
+    }  
+    /* It provides list of employees in model object */  
+    @RequestMapping("/viewemp")  
+    public String viewemp(Model m){  
+        List<Emp> list=dao.getEmployees();  
+        m.addAttribute("list",list);
+        return "viewemp";  
+    }  
+    /* It displays object data into form for the given id.  
+     * The @PathVariable puts URL data into variable.*/  
+    @RequestMapping(value="/editemp/{id}")  
+    public String edit(@PathVariable int id, Model m){  
+        Emp emp=dao.getEmpById(id);  
+        m.addAttribute("command",emp);
+        return "empeditform";  
+    }  
+    /* It updates model object. */  
+    @RequestMapping(value="/editsave",method = RequestMethod.POST)  
+    public String editsave(@ModelAttribute("emp") Emp emp){  
+        dao.update(emp);  
+        return "redirect:/viewemp";  
+    }  
+    /* It deletes record for the given id in URL and redirects to /viewemp */  
+    @RequestMapping(value="/deleteemp/{id}",method = RequestMethod.GET)  
+    public String delete(@PathVariable int id){  
+        dao.delete(id);  
+        return "redirect:/viewemp";  
+    }   
+    
+    @RequestMapping(value="/employeelist",method = RequestMethod.GET,consumes=MediaType.APPLICATION_JSON_VALUE, produces=MediaType.APPLICATION_JSON_VALUE)  
+    public @ResponseBody EmployeeObj getEmployeeList(){  
+    	EmployeeObj empObj=new EmployeeObj();
+    	List<Emp> eList=new ArrayList<Emp>();
+        for(int i=1; i<100;i ++)
+        {
+        	Emp emp =new Emp();
+        	emp.setId(i);
+        	emp.setName("Name" +i);
+        	emp.setDesignation("Manager");
+        	emp.setSalary(1000);
+        	
+        	eList.add(emp);
+        }
+        empObj.setEmployeeList(eList);
+        
+        return empObj;
+    } 
+    
+    @RequestMapping (value="/employeeregistration" , method=RequestMethod.POST, consumes=MediaType.APPLICATION_JSON_VALUE, produces=MediaType.APPLICATION_JSON_VALUE)
+    public @ResponseBody String registerEmployee (@RequestBody Emp employee)
+    {
+    	String return_value="Registration Successful";
+    	
+    	return return_value;
+    }
+}  
