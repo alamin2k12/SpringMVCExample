0485b025a25b329cab04b4c00d663ea7167d9bab
ahossain <ahossain@lrglobalbd.com>
2020-01-13 12:07:05 +0600

    first commit from dev-branch

--- src/main/java/com/api/dao/EmpDao.java
+++ src/main/java/com/api/dao/EmpDao.java
@@ -1,11 +1,12 @@
-package com.javatpoint.dao;  
+package com.api.dao;  
 import java.sql.ResultSet;  
 import java.sql.SQLException;  
 import java.util.List;  
 import org.springframework.jdbc.core.BeanPropertyRowMapper;  
 import org.springframework.jdbc.core.JdbcTemplate;  
-import org.springframework.jdbc.core.RowMapper;  
-import com.javatpoint.beans.Emp;  
+import org.springframework.jdbc.core.RowMapper;
+
+import com.api.model.Emp;  
   
 public class EmpDao {  
 JdbcTemplate template;  
--- src/main/java/com/api/model/EmployeeObj.java
+++ src/main/java/com/api/model/EmployeeObj.java
@@ -1,4 +1,4 @@
-package com.javatpoint.beans;
+package com.api.model;
 
 import java.util.ArrayList;
 import java.util.List;
